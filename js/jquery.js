adf170a046d564772634cd8243b8425071e07af9
Jeff Haupt <jhaupt@southbendin.gov>
2021-03-14 16:07:50 -0400

    updated modals and scss files

--- js/jquery.js
+++ js/jquery.js
@@ -11,4 +11,12 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+
+    $(document).on( "click", "#reserveButton", function() {
+        $("#reserveModal").modal("show");
+    });
+
+    $(document).on( "click", "#loginButton", function() {
+        $("#loginModal").modal("show");
+    });
 });
